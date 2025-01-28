073e9a9a2b7b2f6d719368ffcb2a8622a1ede31d
Dishant Patel <dishant.patel1007@gmail.com>
2025-01-28 15:32:55 +0530

    Update Notice
    https://3.basecamp.com/5798509/buckets/40550626/card_tables/cards/8262580152

--- public/class-advanced-username-manager-public.php
+++ public/class-advanced-username-manager-public.php
@@ -165,7 +165,7 @@ class Advanced_Username_Manager_Public {
 			$aum_general_settings['user_roles'] = array_merge( $aum_general_settings['user_roles'], ['administrator']);
 		}
 		if( is_array($aum_general_settings['user_roles']) && empty( array_intersect( $current_user_roles, $aum_general_settings['user_roles']) ) ) {
-			return esc_html__( 'you are not allow to change the username', 'advanced-username-manager' );
+			return esc_html__( 'You are not allowed to change the username. Please contact the administrator for assistance.', 'advanced-username-manager' );
 		}		
 		ob_start();
 		?>
